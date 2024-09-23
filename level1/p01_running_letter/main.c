dae1883080d8caee797c2bef430f077ff903553d
BeiDAmen_Linyu <3448265184@qq.com>
2024-09-23 19:54:40 +0800

    p01 in level1 finished.

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,29 @@
-#include <printf.h>
+#include <stdio.h>
+#include <windows.h>
+#include <stdbool.h>
 
 int main() {
-    printf("hello world!\n");
+    char str[] = "Uestc";
+    int len = 5;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int ret;
+    ret = GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    int col = csbi.srWindow.Right - csbi.srWindow.Left;
+    int c = 0, t = 1;
+    while (true) {
+        system("cls");
+        if (c < 0) {
+            t = 1;
+        }
+        if (c + len == col) {
+            t = -1;
+        }
+        for (int i = 1; i <= c; i++) {
+            putchar(' ');
+        }
+        printf("%s", str);
+        Sleep(50);
+        c += t;
+    }
     return 0;
 }
