d6b178f605af45a9b4af90823a761a4a085e9ea4
BeiDAmen_Linyu <3448265184@qq.com>
2024-09-30 17:58:19 +0800

    p06 in level1 finished.

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,16 @@
-#include <printf.h>
+#include <stdio.h>
+
+void Hanoi(int n, char scr, char tar, char tmp) {
+    if (n == 1) {
+        printf("%c -> %c\n", scr, tar);
+        return;
+    }
+    Hanoi(n - 1, scr, tmp, tar);  // 上 n - 1 个
+    printf("%c -> %c\n", scr, tar);    // 最下面
+    Hanoi(n - 1, tmp, tar, scr);
+}
 
 int main() {
-    printf("hello world!\n");
+    Hanoi(64, 'A', 'C', 'B');
     return 0;
 }
